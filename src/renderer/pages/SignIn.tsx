3532ff6fbdf0ae74053a592cf3355ad6f8af744d
Mantton <mantton.official@gmail.com>
2022-07-04 14:54:57 -0400

    fix - temporarily remove image tags

--- src/renderer/pages/SignIn.tsx
+++ src/renderer/pages/SignIn.tsx
@@ -1,40 +1,42 @@
 import '../App.css';
-import church from 'assets/icons/church.png';
-
 
 export default function SignInView() {
   return (
     <div>
-    <div id="container">
-      <div id = "contents">
-         <img src={church} alt="church" id="Image"></img>
-        <p> Tax Registration Program</p>
-      
-      </div>
-    </div>
-    <div id="testing">
-    
-      <div id="titles">
-        <img src="assets\icons\form.png" id="icon"></img>
-     
-        <p id="subheading">Login</p>
-      </div>
-    <div id="form_contents">
-
-      <form action="" className="signInForm" id ="form">
-        <input id="elements" placeholder = "username" type="text" /><br></br>
-        
-
-        <input id="elements" placeholder="password" type="text" /><br></br>
-         <button type="button" id="main__btn">Submit</button><br></br><br></br>
-        <button type="button" id="forgot"> Forgot Password? </button><br></br><br></br>
-        <div id="forget_text">
-          Not Registered? Create an Account
+      <div id="container">
+        <div id="contents">
+          <img src="" alt="church" id="Image"></img>
+          <p> Tax Registration Program</p>
         </div>
-      </form>
-      
-    </div>
-    </div>
+      </div>
+      <div id="testing">
+        <div id="titles">
+          <img src="assets/icons/form.png" id="icon"></img>
+
+          <p id="subheading">Login</p>
+        </div>
+        <div id="form_contents">
+          <form action="" className="signInForm" id="form">
+            <input id="elements" placeholder="username" type="text" />
+            <br></br>
+
+            <input id="elements" placeholder="password" type="text" />
+            <br></br>
+            <button type="button" id="main__btn">
+              Submit
+            </button>
+            <br></br>
+            <br></br>
+            <button type="button" id="forgot">
+              {' '}
+              Forgot Password?{' '}
+            </button>
+            <br></br>
+            <br></br>
+            <div id="forget_text">Not Registered? Create an Account</div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
