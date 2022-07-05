2a18cfbf622b90dc886b130d19909e2f8d934e0a
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-07-05 14:30:30 -0400

    FixedtextinLandingPage

--- src/renderer/pages/SignIn.tsx
+++ src/renderer/pages/SignIn.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { getAccount, signIn, useAuthContext } from '../funcs/auth';
 import '../App.css';
-import logo from './church_logo.png';
-import icon from '../../../assets/icon.svg';
+
 
 export default function SignInView() {
   const [email, setEmail] = useState<string>('');
@@ -34,11 +33,7 @@ export default function SignInView() {
         <div id="body_stuff">
           <div id="testing">
             <div id="titles">
-              <img
-                src="assets/icons/form.png"
-                id="icon"
-                alt="image_alternative"
-              />
+              
 
               <h1 id="icon">
                 <i className="bi bi-journal-plus" />
@@ -71,15 +66,18 @@ export default function SignInView() {
                   value="Submit"
                   disabled={working}
                 />
+
               </form>
               <br />
               <br />
+              <div id = "lower_form">
               <button type="button" id="forgot">
                 Forgot Password?
               </button>
               <br />
               <br />
               <div id="forget_text">Not Registered? Create an Account</div>
+              </div>
             </div>
           </div>
         </div>
