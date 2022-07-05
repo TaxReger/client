641f90a49d949c45f34fc39335f45ba7d301c085
Mantton <mantton.official@gmail.com>
2022-07-05 13:15:22 -0400

    run linter

--- src/renderer/pages/Provinces.tsx
+++ src/renderer/pages/Provinces.tsx
@@ -1,37 +1,37 @@
-import '../Provinces.css';
+import './Provinces.css';
 
 export default function SignInView() {
   return (
     <div>
-    <div id="container">
-    <div id = "contents">
-       <img src="assets\icons\church.png" alt="church" id="Image"></img>
+      <div id="container">
+        <div id="contents">
+          <img src="assets\icons\church.png" alt="church" id="Image" />
 
-      <p> Tax Registration Program</p>
-      
+          <p> Tax Registration Program</p>
+        </div>
+      </div>
+      <div>
+        <div id="testing">
+          <div>
+            <img src="assets\icons\map.png" id="icon" alt="map icon" />
+            <p id="header"> Province </p>
+          </div>
+
+          <form action="" className="signInForm" id="form">
+            <select name="Provinces" id="provinces">
+              <option value="volvo">Quebec</option>
+              <option value="volvo">Ontario</option>
+            </select>
+
+            <br />
+            <button type="button" id="main__btn">
+              Next
+            </button>
+            <br />
+            <br />
+          </form>
+        </div>
+      </div>
     </div>
-  </div>
-<div>
-<div id = "testing">
-    <div>
-      <img src="assets\icons\map.png" id="icon" alt = "map icon"></img>
-      <p id = "header"> Province </p>
-     
-    </div>
-
-    <form action="" className="signInForm" id ="form">
-      <select name="Provinces" id="provinces">
-<option value="volvo">Quebec</option>
-        <option value="volvo">Ontario</option>
-        
-      </select>
-      
-
-     <br></br>
-       <button type="button" id="main__btn">Next</button><br></br><br></br>
-       </form>
-      </div>
-      </div>
-      </div>
   );
 }
--- src/renderer/pages/Signup.tsx
+++ src/renderer/pages/Signup.tsx
@@ -1,45 +1,40 @@
-import '../Signup.css';
+import './Signup.css';
 
 export default function SignUpView() {
   return (
     <div>
-<nav className="navbar navbar-light bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand text-light" href="#">Tax Registration Program </a>
-    
-    
-  </div>
-</nav>
+      <nav className="navbar navbar-light bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light" href="/">
+            Tax Registration Program{' '}
+          </a>
+        </div>
+      </nav>
 
-  
+      <div id="body_stuff">
+        <div id="testing">
+          <div id="titles">
+            <h1 id="icon">
+              <i className="bi bi-person-plus" />
+            </h1>
 
+            <p id="subheading">Sign Up</p>
+          </div>
+          <div id="form_contents">
+            <form action="" className="signInForm" id="form">
+              <input id="elements" placeholder="username" type="text" />
+              <br />
 
-  <div id="body_stuff">
-
-    <div id="testing">
-    
-      <div id="titles">
-        
-        <h1 id = "icon"><i className="bi bi-person-plus"></i></h1>
-       
-     
-        <p id="subheading">Sign Up</p>
-        
+              <input id="elements" placeholder="password" type="text" />
+              <br />
+              <button type="button" id="main__btn">
+                Save
+              </button>
+              <br />
+            </form>
+          </div>
+        </div>
       </div>
-    <div id="form_contents">
-
-      <form action="" className="signInForm" id ="form">
-        <input id="elements" placeholder = "username" type="text" /><br></br>
-        
-
-        <input id="elements" placeholder="password" type="text" /><br></br>
-         <button type="button" id="main__btn">Save</button><br></br>
-      
-      </form>
-      
     </div>
-    </div>
-  </div>
-  </div>
   );
 }
