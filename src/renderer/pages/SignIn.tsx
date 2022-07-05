2d90e04742ca974b149e61ac8caf887a462164b2
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-07-05 10:25:32 -0400

    NewLandingPagewithBootstrap

--- src/renderer/pages/SignIn.tsx
+++ src/renderer/pages/SignIn.tsx
@@ -1,42 +1,52 @@
 import '../App.css';
 
+
 export default function SignInView() {
   return (
-    <div>
-      <div id="container">
-        <div id="contents">
-          <img src=""alt="church" id="Image"></img>
-          <p> Tax Registration Program</p>
-        </div>
-      </div>
-      <div id="testing">
-        <div id="titles">
-          <img src="assets/icons/form.png" id="icon" alt=" form image"></img>
+    <div id = "main_body">
 
-          <p id="subheading">Login</p>
-        </div>
-        <div id="form_contents">
-          <form action="" className="signInForm" id="form">
-            <input id="elements" placeholder="username" type="text" />
-            <br></br>
-
-            <input id="elements" placeholder="password" type="text" />
-            <br></br>
-            <button type="button" id="main__btn">
-              Submit
-            </button>
-            <br></br>
-            <br></br>
-            <button type="button" id="forgot">
-              {' '}
-              Forgot Password?{' '}
-            </button>
-            <br></br>
-            <br></br>
-            <div id="forget_text">Not Registered? Create an Account</div>
-          </form>
-        </div>
-      </div>
+    <nav className="navbar navbar-light bg-dark" >
+      <div className="container-fluid">
+        <a className="navbar-brand text-light" href="#">Tax Registration Program</a>
+      
+      
+      
     </div>
+  </nav>
+  
+    
+  
+  
+    <div id="body_stuff">
+  
+      <div id="testing">
+      
+        <div id="titles">
+          
+          <h1 id = "icon"><i className="bi bi-journal-plus"></i></h1>
+         
+       
+          <p id="subheading">Login</p>
+          
+        </div>
+      <div id="form_contents">
+  
+        <form action="" className="signInForm" id ="form">
+          <input id="elements" placeholder = "username" type="text" /><br></br>
+          
+  
+          <input id="elements" placeholder="password" type="text" /> <br></br>
+           <button type="button" id="main__btn">Submit</button><br></br><br></br>
+          <button type="button" id="forgot"> Forgot Password? </button><br></br><br></br>
+          <div id="forget_text">
+            Not Registered? Create an Account
+          </div>
+        </form>
+        
+      </div>
+      </div>
+      </div>
+      </div>
+
   );
 }
