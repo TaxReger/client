2348d6e7427336384f636ce706fcd42cc93ef518
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-07-05 13:39:52 -0400

    ProvincewithBootstrap

--- src/renderer/pages/Provinces.tsx
+++ src/renderer/pages/Provinces.tsx
@@ -3,35 +3,45 @@ import '../Provinces.css';
 export default function SignInView() {
   return (
     <div>
-    <div id="container">
-    <div id = "contents">
-       <img src="assets\icons\church.png" alt="church" id="Image"></img>
+<nav className="navbar navbar-light bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand text-light" href="#">Tax Registration Program </a>
+     <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Sign out</button>
+    
+    
+  </div>
+</nav>
 
-      <p> Tax Registration Program</p>
+  
+  <div id="body_stuff">
+
+    <div id="testing">
+    
+      <div id="titles">
+        
+        <h1 id = "icon"><i className="bi bi-geo-alt"></i></h1>
+       
+     
+        <p id="subheading">Provinces</p>
+        
+      </div>
+    <div id="form_contents">
+
+       <form action="" className="signInForm" id ="form">
+        <select name="Provinces" id="provinces">
+  <option value="volvo">Quebec</option>
+          <option value="volvo">Ontario</option>
+          
+        </select>
+        </form>
+        
+
+       <br></br>
+         <button type="button" id="main__btn">Next</button><br></br><br></br>
+        </div>
       
+    </div>
     </div>
   </div>
-<div>
-<div id = "testing">
-    <div>
-      <img src="assets\icons\map.png" id="icon" alt = "map icon"></img>
-      <p id = "header"> Province </p>
-     
-    </div>
-
-    <form action="" className="signInForm" id ="form">
-      <select name="Provinces" id="provinces">
-<option value="volvo">Quebec</option>
-        <option value="volvo">Ontario</option>
-        
-      </select>
-      
-
-     <br></br>
-       <button type="button" id="main__btn">Next</button><br></br><br></br>
-       </form>
-      </div>
-      </div>
-      </div>
   );
 }
