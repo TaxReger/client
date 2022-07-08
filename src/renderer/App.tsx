e35d23794b2df9f02f9d7b523098ac8520625bfa
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-07-08 09:30:12 -0400

    updatedApp

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,13 +3,6 @@ import './SignIn.css';
 import ProvinceView from './pages/purchase/Provinces';
 import SignInView from './pages/authentication/SignIn';
 import TableIntroView from './pages/TableIntro';
-<<<<<<< Updated upstream
-import PasswordResetView from './pages/authentication/PasswordReset';
-import ExportPage from './pages/export/ExportPage';
-import SearchPage from './pages/search/SearchPage';
-import { useAuthContext } from './funcs/auth';
-import SignUpView from './pages/authentication/Signup';
-=======
 import TableView from './pages/TableView';
 import PasswordResetView from './pages/PasswordReset';
 import StoreView from './pages/Store';
@@ -17,7 +10,6 @@ import SearchStoreView from './pages/searchstore';
 import OntarioFormView from './pages/OntarioForm';
 import QuebecFormView from './pages/QuebecForm';
 import InvoicesearchView from './pages/Invoicesearch';
->>>>>>> Stashed changes
 
 const Gateway = () => {
   const { account } = useAuthContext();
