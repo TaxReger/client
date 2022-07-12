d5a088d9e3bd58a5e9d7876ce83985756350c627
Nodshley Marcelin <nmarc068@uottawa.ca>
2022-07-12 13:20:58 -0400

    CreatedReportForm

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -11,6 +11,7 @@ import StoreView from './pages/purchase/store/Store';
 import AddStorePage from './pages/purchase/store/newStore/AddStore';
 import AddPurchasePage from './pages/purchase/newPurchase/NewPurchase';
 import SearchStoreView from './pages/search/searchstore';
+import ReportFormView from './pages/ReportForm';
 
 const Gateway = () => {
   const { account } = useAuthContext();
